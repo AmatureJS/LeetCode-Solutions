9ec959dadf54e327aa3280b9b47e2e0f293a2e5b
AmatureJS <40333427+AmatureJS@users.noreply.github.com>
2024-01-27 00:50:32 +0530

    Update product-sales-analysis-iii.sql

--- MySQL/product-sales-analysis-iii.sql
+++ MySQL/product-sales-analysis-iii.sql
@@ -1,10 +1,21 @@
-# Time:  O(n) 
-# Space: O(n) 
+WITH
+  cte_first_year AS (
+    SELECT
+      product_id,
+      year,
+      quantity,
+      price,
+      RANK() OVER (PARTITION BY product_id ORDER BY year) AS year_number
+    FROM
+      Sales
+  )
 
-SELECT product_id, 
-       Min(year) AS first_year, 
-       quantity, 
-       price 
-FROM   sales 
-GROUP  BY product_id 
-ORDER  BY NULL 
+SELECT
+  product_id,
+  year AS first_year,
+  quantity,
+  price
+FROM
+  cte_first_year
+WHERE
+  year_number = 1
